5c771f401f49323b92614f3972ae73c06a337f9e
AlejandroFlores004 <alejandroflores004@outlook.com>
2025-08-27 13:55:15 -0600

    Generación de usuario

---
+++ user/forms.py
@@ -0,0 +1,19 @@
+# forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class userForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'groups','password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User,Group
+from .forms import userForm
 
 # Create your views here.
 def userMain(request):
@@ -11,3 +12,17 @@ def userMain(request):
         "grupos" : groups
     }
     return render(request,'usuarioMain.html',objects)
+
+
+def userFormView(request):
+    if request.method == 'POST':
+        form = userForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = userForm()
+
+    object = {
+        "form" : form,
+    }
+    return render(request, 'usuarioForm.html', object)
