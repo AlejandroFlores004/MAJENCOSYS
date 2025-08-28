f81b7ecb3592b080326e88f716bd7e8bf20a20fa
AlejandroFlores004 <alejandroflores004@outlook.com>
2025-08-28 11:25:25 -0600

    Pequeños detalle visuales

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,Group
 from .forms import userForm
+from django.contrib import messages
 
 # Create your views here.
 def userMain(request):
@@ -19,6 +20,8 @@ def userFormView(request):
         form = userForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, '!El usuario se guardo exitosamente')
+            return redirect('usuariosMain')
     else:
         form = userForm()
 
