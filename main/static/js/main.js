0538644eef71f372737a8e2caab129f4a0413d2b
Ian21023 <pm21023@ues.edu.sv>
2025-08-31 13:47:49 -0600

    Actualizacion Base Cliente y Workflow

---
+++ main/static/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    
+})(jQuery);
+
