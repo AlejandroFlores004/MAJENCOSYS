eca708158084439881f9bd14a10e562d1cfd30b4
AlejandroFlores004 <alejandroflores004@outlook.com>
2025-09-03 11:11:42 -0600

    Inicio de sesión sencillo

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import startpage,dashboard
+from .views import startpage,dashboard,loginPage
 
 urlpatterns = [
     path('', startpage, name='startpage'),
     path('dashboard', dashboard, name='dash'),
+    path('login/',loginPage, name='log'),
     path('admon_usuarios/',include('user.urls'),name='user')
 ]
