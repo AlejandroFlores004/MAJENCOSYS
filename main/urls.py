0db0a0a4db2f370e8b4cff06b1d63d99946abcd2
AlejandroFlores004 <alejandroflores004@outlook.com>
2025-09-06 14:51:24 -0600

    Desatillitos

--- main/urls.py
+++ main/urls.py
@@ -6,29 +6,24 @@ from .forms import CustomSetPasswordForm,CustomPasswordResetForm
 
 urlpatterns = [
     path('', startpage, name='startpage'),
-    path('dashboard/', dashboard, name='dash'),
+    path('dashboard/', dashboard, name='dashboard'),
     path('login/', loginPage, name='log'),
     path('admon_usuarios/', include('user.urls'), name='user'),
 
     # ---- Password Reset Flow ----
-    path('reset-password/', 
-         auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html',
-         form_class=CustomPasswordResetForm), 
-         name='password_reset'),
-
-    path('reset-password/done/', 
-         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), 
-         name='password_reset_done'),
-
+     path('reset-password/', 
+          auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html',
+          form_class=CustomPasswordResetForm), 
+          name='password_reset'),
+     path('reset-password/done/', 
+          auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), 
+          name='password_reset_done'),
      path('reset-password/<uidb64>/<token>/',
           auth_views.PasswordResetConfirmView.as_view(
-        template_name='registration/password_reset_confirm.html',
-        form_class=CustomSetPasswordForm
-     ),
-    name='password_reset_confirm'
-),
-
-    path('reset-password/complete/', 
-         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), 
-         name='password_reset_complete'),
+          template_name='registration/password_reset_confirm.html',
+          form_class=CustomSetPasswordForm),
+          name='password_reset_confirm'),
+     path('reset-password/complete/', 
+          auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), 
+          name='password_reset_complete'),
 ]
