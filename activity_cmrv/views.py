2bef6cade04b5b998a1a2b1c0c2560c9af66d5c4
AlejandroFlores004 <alejandroflores004@outlook.com>
2025-10-08 10:16:43 -0600

    Estos cambios ya no se si quiera funcionan

--- activity_cmrv/views.py
+++ activity_cmrv/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from project.models import Project
 from django.shortcuts import get_object_or_404
+from .models import Activity, memoryHeavyMachine, memoryLabour, memoryMaterial, memoryTool
 # Create your views here.
 def mainActivityCrmv(request, pk):
     project = get_object_or_404(Project, pk=pk)
-    objects = {
+    activities = Activity.objects.filter(project=project)
+    materials = memoryMaterial.objects.filter(activity__in=activities)
+    labours = memoryLabour.objects.filter(activity__in=activities)
+    tools = memoryTool.objects.filter(activity__in=activities)
+    
+    return render(request, 'mainActivityInfraestructura.html', {
         'project': project,
-    }
-    return render(request, 'mainActivityCmrv.html', objects)
+        'activities': activities,
+        'materials': materials,
+        'labours': labours,
+        'tools': tools,
+    })
+
