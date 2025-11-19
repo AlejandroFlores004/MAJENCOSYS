5ab69f42ddb19664ba71f19acc325ee074803d19
AlejandroFlores004 <alejandroflores004@outlook.com>
2025-11-18 22:20:27 -0600

    Remove unused ready method from UserConfig to simplify app initialization

--- user/apps.py
+++ user/apps.py
@@ -5,6 +5,3 @@ class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
 
-    def ready(self):
-        from .models import create_default_groups
-        create_default_groups()
